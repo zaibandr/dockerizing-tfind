8d90bbcc90f5630df69f3b9ee3b0903915c482a5
zaibandr <zaibandr@ya.ru>
2017-06-13 21:11:16 +0300

    Update admin.py

--- web/docker_django/apps/find_torrent/admin.py
+++ web/docker_django/apps/find_torrent/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Torrent, 
+from .models import Torrent, Trend
 
 
-@admin.register(Torrent)
-class TorrentAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Trend)
-class TrendAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(Torrent)
+admin.site.register(Trend)
 
